248e8b137bbd554335780e11cbd5e13f88581982
Enore964 <enore.vidotto@gmail.com>
2026-01-19 17:51:01 +0100

    app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def login():
 if login():
     # --- DATI PORTAFOGLIO ---
     LISTA_TITOLI = {
-        "URA":  {"t": "URAM.MI",   "acq": 54.07,  "q": 200,  "n": "Uranio Milano", "corr": 1.094},  
+        "URA":  {"t": "URAM.MI",   "acq": 54.07,  "q": 200,  "n": "Uranio Milano", "corr": 1.0},  
         "LDO":  {"t": "LDO.MI",    "acq": 59.855, "q": 200,  "n": "Leonardo",      "corr": 1.0},  
         "EXA":  {"t": "EXAI.MI",   "acq": 1.9317, "q": 3000, "n": "Expert AI",     "corr": 1.0},   
         "AVI":  {"t": "AVIO.MI",   "acq": 39.1,   "q": 250,  "n": "Avio Spazio",   "corr": 1.0},
@@ -169,6 +169,7 @@ if login():
     if st.sidebar.button("Logout"):
         st.session_state["p_ok"] = False
         st.rerun()
+
 
 
 
